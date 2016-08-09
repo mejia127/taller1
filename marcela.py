b3be1791f06a64fb36c0891b2cc159ae3604ec1d
mejia127 <marcemejia127@hotmail.com>
2016-08-08 20:34:30 -0500

    crecion proyecto 1

---
+++ marcela.py
@@ -0,0 +1,101 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print "hola mundo"
+
+
+# $$\int_x^\infty\frac{{dt}}{t(t^2-1)log(t)}$$
+# 
+
+# In[3]:
+
+2 + 4 * 6 / 8.0 -3
+
+
+# In[4]:
+
+3/2
+
+
+# In[5]:
+
+3+5j
+
+
+# In[6]:
+
+(3+5j)-(1-1j)
+
+
+# In[7]:
+
+abs(3+5j)
+
+
+# In[8]:
+
+5**3
+
+
+# In[9]:
+
+int(_7)
+
+
+# In[11]:
+
+float(_8)
+
+round(_7)
+# In[12]:
+
+round(_7)
+
+
+# In[13]:
+
+complex(_9)
+
+
+# In[16]:
+
+max(1,3,-5)
+
+
+
+# min(1,3,-5
+
+# In[17]:
+
+min(1,5,-5)
+
+
+# In[19]:
+
+x,y,z,a =1,2,3,4
+
+
+# In[20]:
+
+print x,y,z,a
+
+
+# In[21]:
+
+print y
+
+
+# In[22]:
+
+print (x); print(y);print(z);print(a)
+
+
+# In[32]:
+
+if x>y:
+    print "el mayor es",x 
+else:
+    print "el menor es",y
+
